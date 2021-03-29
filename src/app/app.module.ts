23b5dc9960e5c2f5c6eb4d4d0f00809861c4f210
modelsofidentity <davide@modelsofidentity.com>
2021-03-28 21:57:13 -0700

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserModule,
   
  RouterModule.forRoot([
-    {path: 'crisis-list', component: CrisisListComponent},
+    {path: 'crisis-list', component: CrisisListComponent},  //here the path tagname and the component class
     {path: 'heroes-list', component: HeroesListComponent}, //here we set the path for this thing
-    {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},  //this creates the home page kind of
-    {path: '**', component: PageNotFoundComponent}
+    {path: '', redirectTo: '/heroes-list', pathMatch: 'full'}  //this creates the home page kind of
+
     
   ]), ],
 
 
-  declarations: [ AppComponent, HelloComponent, CrisisListComponent, HeroesListComponent, PageNotFoundComponent ],
+  declarations: [ AppComponent, HelloComponent, CrisisListComponent, HeroesListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
